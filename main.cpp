f38244355890b4b3904f4f8b249c5cd4267b4b19
maybe0hiren <hirenwaghmare25@gmail.com>
2025-06-27 01:02:46 +0530

    App completed

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "mainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+    return app.exec();
+}
